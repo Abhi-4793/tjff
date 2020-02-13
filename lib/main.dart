c4990ec46915b897335861bcee320529420f305b
Abhi-4793 <abhinavkrishnapy2@gmail.com>
2020-02-13 23:55:13 +0530

    MyApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,19 @@
-immport 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
 main() => runApp(MyApp());
-
-class MyApp  extends StatelessWidget{
+  class MyApp  extends StatelessWidget {
   @override
   Widget build(BuildContext context){
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("hello world"))
-        body: Column(
-          children: <Widget>[
-            Text("hello world")
-
-          ], // <Widget>[]
-        ), //column
-      ), //Scaffold
+          appBar: AppBar(title: Text("hello world"),),
+          body: Column(
+              children: <Widget>
+              [
+                Text("Heyy myself abhinav")
+              ], // <Widget>[]
+          ), //column
+    ), //Scaffold
     );  //MaterialApp
   }
-}
+ }
